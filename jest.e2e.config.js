9a15175f7c2eae19b9dfff7d794386ea9bf43f37
Bill Chen <3580951+gfxblit@users.noreply.github.com>
2025-12-30 22:07:51 -0800

    renable parallel

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -2,6 +2,5 @@ export default {
   testEnvironment: 'node',
   testMatch: ['**/e2e/**/*.test.js'],
   transform: {},
-  testTimeout: 30000,
-  maxWorkers: 1 // Run tests serially to avoid port conflicts with Vite server
+  testTimeout: 30000
 };
